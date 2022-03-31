9ab3fec1bb3bb0d712ab86fae58f3b6e91b26097
ChanServy <1579700161@qq.com>
2022-03-31 13:39:06 +0800

    秒杀、一人一单业务完善

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -48,7 +48,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
      * @return Result
      */
     @Override
-    @Transactional//事务，保证原子性，减库存和生成订单两个操作同时成功、同时失败，单机项目这么用，分布式项目微服务间要用分布式事务
+    // @Transactional
     public Result seckill(Long voucherId) {
         //根据id查询秒杀优惠券信息
         SeckillVoucher seckillVoucher = seckillVoucherMapper.selectById(voucherId);
@@ -68,8 +68,21 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足！");
         }
 
-        VoucherOrder voucherOrder = new VoucherOrder();
+        VoucherOrder voucherOrder = updateStockAndSaveOrder(voucherId);
+        if (voucherOrder == null) {
+            return Result.fail("下单失败！");
+        }
+        return Result.ok(voucherOrder);
+    }
 
+    /**
+     * 单体项目下，一人一单业务可以使用synchronized锁
+     *
+     * @param voucherId 优惠券id
+     * @return 订单对象VoucherOrder
+     */
+    @Transactional//事务，保证原子性，减库存和生成订单两个操作同时成功、同时失败，单机项目这么用，分布式项目微服务间要用分布式事务
+    public VoucherOrder updateStockAndSaveOrder(Long voucherId) {
         //每人同样的优惠券只能购买一次，一人一单，是对来自同一个用户的并发抢购请求进行判断，判断其线程安全问题
         //同一个用户的请求来了，我们才去判断并发安全问题，如果并发请求不是来自同一个用户，就不需要加锁，因此锁对象选择用户id
         //我们期望的是用户id值一样的请求用同一把锁。
@@ -83,48 +96,50 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             queryWrapper.eq("user_id", userId);
             queryWrapper.eq("voucher_id", voucherId);
             Integer count = voucherOrderMapper.selectCount(queryWrapper);
+            //user_id用户之前已经对voucher_id优惠券抢购过了，不能重复下单
             if (count > 0) {
-                return Result.fail("不能重复购买哦！");
+                return null;
             }
             /*
-            Mybatis的方式实现
-            // 注：这块不能用  seckillVoucher.getStock()-1，因为是高并发，不能通过上面查到的库存值传递计算，要直接基于数据库层面使用sql运算！！！
-            // mysql连接驱动的更新操作默认返回的并不是受影响的行数，如果想设置返回值是受影响的行数，修改数据库链接配置：增加useAffectedRows=true
-            int modified = seckillVoucherMapper.updateStock(voucherId);
-            if (modified != 1) {
-                return Result.fail("库存不足！");
-            }
+                Mybatis的方式实现
+                // 注：这块不能用  seckillVoucher.getStock()-1，因为是高并发，不能通过上面查到的库存值传递计算，要直接基于数据库层面使用sql运算！！！
+                // mysql连接驱动的更新操作默认返回的并不是受影响的行数，如果想设置返回值是受影响的行数，修改数据库链接配置：增加useAffectedRows=true
+                int modified = seckillVoucherMapper.updateStock(voucherId);
+                if (modified != 1) {
+                    return Result.fail("库存不足！");
+                }
             */
 
             /*
-            Mybatis-plus实现，基于Service层的接口
-            // 注：这块不能用  seckillVoucher.getStock()-1  ！！！
-            boolean success = seckillVoucherService.update().setSql("stock = stock - 1")
-                    .eq("voucher_id", voucherId).gt("stock", 0).update();
-            if (!success) {
-                return Result.fail("库存不足！");
-            }
+                Mybatis-plus实现，基于Service层的接口
+                // 注：这块不能用  seckillVoucher.getStock()-1  ！！！
+                boolean success = seckillVoucherService.update().setSql("stock = stock - 1")
+                        .eq("voucher_id", voucherId).gt("stock", 0).update();
+                if (!success) {
+                    return Result.fail("库存不足！");
+                }
             */
 
             // Mybatis-plus实现，基于Mapper层的接口
+            UpdateWrapper<SeckillVoucher> updateWrapper = new UpdateWrapper<>();
             // 注：这块不能用  seckillVoucher.getStock()-1，因为是高并发，不能通过上面查到的库存值传递计算，要直接基于数据库层面使用sql运算！
             // 因此只能使用.setSql("sql语句")，不能用.set()方法，也不能是new对象设置属性值的方式。
-            // mysql连接驱动的更新操作默认返回的并不是受影响的行数，如果想设置返回值是受影响的行数，修改数据库链接配置：增加useAffectedRows=true
-            UpdateWrapper<SeckillVoucher> updateWrapper = new UpdateWrapper<>();
             updateWrapper.setSql("stock = stock - 1").eq("voucher_id", voucherId).gt("stock", 0);
+            // mysql连接驱动的更新操作默认返回的并不是受影响的行数，如果想设置返回值是受影响的行数，修改数据库链接配置：增加useAffectedRows=true
             int modified = seckillVoucherMapper.update(null, updateWrapper);
+            //库存不足
             if (modified != 1) {
-                return Result.fail("库存不足！");
+                return null;
             }
-
             //创建订单
-
+            VoucherOrder voucherOrder = new VoucherOrder();//要在synchronized里面new
             voucherOrder.setId(idWorker.nextId("order"));
             voucherOrder.setUserId(userId);
             voucherOrder.setVoucherId(voucherId);
             //订单入库
             voucherOrderMapper.insert(voucherOrder);
+            //返回订单
+            return voucherOrder;
         }
-        return Result.ok(voucherOrder);
     }
 }
