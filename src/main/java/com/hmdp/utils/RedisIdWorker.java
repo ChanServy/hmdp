7cd23c2e8f2b6167fde7f12fa9c3b9c055f8f2c5
ChanServy <1579700161@qq.com>
2022-08-22 17:51:40 +0800

    添加redis全局唯一id生成器注释

--- src/main/java/com/hmdp/utils/RedisIdWorker.java
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -17,6 +17,9 @@ public class RedisIdWorker {
     /*LocalDateTime time = LocalDateTime.of(2022, 1, 1, 0, 0, 0);
     long second = time.toEpochSecond(ZoneOffset.UTC);
     second = 1640995200*/
+    /**
+     * 开始时间戳
+     */
     private static final long BEGIN_TIMESTAMP = 1640995200L;
     private static final int COUNT_BITS = 32;
 
@@ -29,12 +32,17 @@ public class RedisIdWorker {
     public long nextId(String keyPrefix) {
         //1.生成时间戳
         LocalDateTime now = LocalDateTime.now();
+        //当前时间戳
         long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        //生成时间戳
         long timestamp = nowSecond - BEGIN_TIMESTAMP;
+
         //2.生成序列号
         String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
-        //将redis中"increment:" + keyPrefix + ":" + date键对应的值自增并返回自增之后的值，起初没有这个键的话，incr后会新建并返回1
-        Long count = stringRedisTemplate.opsForValue().increment("incr:" + keyPrefix + ":" + date);//redis的incr为原子操作
+        //将redis中 "increment:" + keyPrefix + ":" + date键 对应的值自增并返回自增之后的值，起初没有这个键的话，incr后会新建并返回1
+        //key最后拼接date，可以统计当天一天的订单量
+        long count = stringRedisTemplate.opsForValue().increment("incr:" + keyPrefix + ":" + date);//redis的incr为原子操作
+
         //3.拼接并返回
         return timestamp << COUNT_BITS | count;
     }
